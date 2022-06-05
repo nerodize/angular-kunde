7302b6881729c2822f1e5df3be3b92aa8108af8e
alexseitz <alexseitz248@gmail.com>
2022-06-05 18:40:43 +0200

    feat: basic layout for footer component

---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import log from 'loglevel';
+
+@Component({
+    selector: 'hs-kunde-footer',
+    template: '',
+})
+export class FooterComponent {
+    constructor() {
+        log.debug('Footermodule.constructor()');
+    }
+}
